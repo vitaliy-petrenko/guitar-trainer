a13b4f472269fef8325371b08ed4ce82fa5a154d
Vitaliy Petrenko <lpdev.vitaliy@gmail.com>
2019-10-16 12:38:52 +0300

    Updates

--- precache-manifest.a785f71f70513743409cdda72f718504.js
+++ precache-manifest.a785f71f70513743409cdda72f718504.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b141664e8d413275311a29d3e064bc75",
+    "revision": "86214eafc3b6516b8b24e9f08ee258c4",
     "url": "/guitar-trainer/index.html"
   },
   {
-    "revision": "a44fd4bf0f069cc9b587",
-    "url": "/guitar-trainer/static/css/main.6c05c3f8.chunk.css"
+    "revision": "d3e780cf7606ea925c9d",
+    "url": "/guitar-trainer/static/css/main.9cf330ad.chunk.css"
   },
   {
     "revision": "6644c53993cd4becb699",
     "url": "/guitar-trainer/static/js/2.1b5c5b28.chunk.js"
   },
   {
-    "revision": "a44fd4bf0f069cc9b587",
-    "url": "/guitar-trainer/static/js/main.591576b1.chunk.js"
+    "revision": "d3e780cf7606ea925c9d",
+    "url": "/guitar-trainer/static/js/main.5bb3a54d.chunk.js"
   },
   {
     "revision": "73fb42da77a221f3dae6",
